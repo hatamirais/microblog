1a9ef5b94c68b10f3c58d0aebea1e85ed15535d4
Hatami Ra'is Bukhari <hatamierbe12@gmail.com>
2024-05-22 11:21:03 +0700

    complete chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Althemier'}
+    posts = [
+        {
+            'author' : {'username': 'John'},
+            'body': 'Post 1'
+        },
+        {
+            'author' : {'username': 'Susan'},
+            'body': 'Post 2'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
